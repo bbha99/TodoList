bd4e6e46b11d56bddcd4cd945f14ea18d3553304
bbha99 <branha99@gmail.com>
2023-03-15 01:43:32 -0700

    add fetch and post requests

--- todo_api_user/src/TodoList/TodoForm.js
+++ todo_api_user/src/TodoList/TodoForm.js
@@ -1,17 +1,63 @@
 import React, {Component} from "react"
+import { TextField, Button } from "@mui/material"
 
 class TodoForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+      api_url: props.api_url,
+      task: "",
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTaskChange = this.handleTaskChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.formSubmit(event.target);
+  }
+
+  async formSubmit(formData) {
+    var data = new FormData(formData);
+    await fetch(this.state.api_url, {
+      method: "POST",
+      mode: "cors",
+      body: data
+    }).then(response => response.json())
+    .then(response => this.props.updateTodoList(response))
+  }
+
+  handleTaskChange(event) {
+    this.setState({
+      task: event.target.value
+    })
   }
 
   render () {
     return (
       <div>
-        <p>This is the form area</p>
+        <form 
+        onSubmit={this.handleSubmit}
+        id="todo_form"
+        autoComplete="off">
+          
+          <TextField 
+          id="task_input"
+          multiline
+          rows={2}
+          label="Task Description"
+          placeholder="Enter a description"
+          variant="outlined"
+          type="text"
+          name="todo[task]"
+          onChange={this.handleTaskChange}/>
+          
+          <Button 
+          variant="contained"
+          color="primary"
+          type="submit">Add Task
+          </Button>
+        </form>
       </div>
     )
   }
--- todo_api_user/src/TodoList/TodoList.js
+++ todo_api_user/src/TodoList/TodoList.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react"
 import TodoForm from "./TodoForm"
+import TodoItem from "./TodoItem"
 
-const api_url = `https://localhost:3001/api/v1/todos`
+const api_url = `http://localhost:3001/api/v1/todos`
 
 class TodoList extends Component {
   constructor(props) {
@@ -9,15 +10,40 @@ class TodoList extends Component {
     this.state = {
       items: []
     }
+    this.updateTodoList = this.updateTodoList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    fetch(api_url)
+    .then(response => response.json())
+    .then(response_items => {
+      this.setState({
+        items: response_items.reverse()
+      })
+    });
+  }
+
+  updateTodoList(item) {
+    let _items = this.state.items
+    _items.unshift(item)
+    this.setState({
+      items:_items
+    })
   }
 
   render () {
+    console.log(this.state.items)
     return (
       <div>
-        <TodoForm />
-        <ul>
-          <li>Todo #1</li>
-          <li>Todo #2</li>
+        <TodoForm api_url={api_url} updateTodoList={this.updateTodoList} />
+        <ul id="todo_list">
+          {this.state.items.map((item) => (
+            <TodoItem key={item.id} item={item} />
+          ))}
         </ul>
       </div>
     )
